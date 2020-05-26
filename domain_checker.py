db66df160011fd342440fcd135a5654a2ecfbd6a
Alexey Suponin <lexxsmith@gmail.com>
2020-05-27 03:57:43 +0700

    error catch, closed #3

--- domain_checker.py
+++ domain_checker.py
@@ -3,14 +3,22 @@
 import argparse
 import socket
 import ssl
+from collections import OrderedDict
 from pprint import pprint
 
 
 def check_domain(domain):
+    result = OrderedDict()
     ctx = ssl.create_default_context()
-    with ctx.wrap_socket(socket.socket(), server_hostname=domain) as s:
-        s.connect((domain, 443))
-        cert = s.getpeercert()
+    with ctx.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_hostname=domain) as s:
+        try:
+            s.settimeout(2)
+            s.connect((domain, 443))
+            cert = s.getpeercert()
+        except Exception as e:
+            result['domain'] = domain
+            result['error'] = e
+            return result
 
     subject = dict(x[0] for x in cert['subject'])
     issued_to = subject['commonName']
@@ -18,17 +26,17 @@ def check_domain(domain):
     issued_by = issuer['commonName']
     expired = cert['notAfter']
     dns = cert['subjectAltName']
-    print(dns)
     certs_names = []
     for k, v in dns:
         certs_names.append(v)
 
-    return {'domain': domain,
-            'alternative_names': certs_names,
-            'issued_to': issued_to,
-            'issued_by': issued_by,
-            'expired': expired
-    }
+    result['domain'] = domain
+    result['alternative_names'] = certs_names
+    result['issued_to'] = issued_to
+    result['issued_by'] = issued_by
+    result['expired'] = expired
+
+    return result
 
 
 def main():
