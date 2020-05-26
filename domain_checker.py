8d70ed9437d243eae440197fd095362bc89786ae
Alexey Suponin <lexxsmith@gmail.com>
2020-05-27 02:31:16 +0700

    Create domain certs checker.py

---
+++ domain_checker.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+# !/usr/bin/env python
+import argparse
+import socket
+import ssl
+from pprint import pprint
+
+
+def check_domain(domain):
+    ctx = ssl.create_default_context()
+    with ctx.wrap_socket(socket.socket(), server_hostname=domain) as s:
+        s.connect((domain, 443))
+        cert = s.getpeercert()
+
+    subject = dict(x[0] for x in cert['subject'])
+    issued_to = subject['commonName']
+    issuer = dict(x[0] for x in cert['issuer'])
+    issued_by = issuer['commonName']
+    expired = cert['notAfter']
+    dns = cert['subjectAltName']
+    print(dns)
+    certs_names = []
+    for k, v in dns:
+        certs_names.append(v)
+
+    return {'domain': domain,
+            'alternative_names': certs_names,
+            'issued_to': issued_to,
+            'issued_by': issued_by,
+            'expired': expired
+    }
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--domain", type=str, help="Do SSL lockup for provided domain", required=True)
+    args = parser.parse_args()
+    result = check_domain(args.domain)
+    pprint(result)
+
+
+if __name__ == '__main__':
+    main()
