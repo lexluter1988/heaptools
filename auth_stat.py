668fad57599b80ea520ce2951b1142e075b552e3
Alexey Suponin <lexxsmith@gmail.com>
2020-09-17 22:47:08 +0700

    Update auth_stat.py

--- auth_stat.py
+++ auth_stat.py
@@ -1,6 +1,7 @@
 import argparse
 import re
 import requests
+from memory_profiler import profile
 
 entry_pattern = r"(\s)*(?P<date>(\(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov)( \d{2} \d{2}:\d{2}:\d{2}))" \
                 r"(.*)(\s)*(?P<ip>(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b))"
@@ -8,6 +9,7 @@ entry_pattern = r"(\s)*(?P<date>(\(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov)(
 endpoint = "https://ipvigilante.com/"
 
 
+@profile
 def log_reader(log_file):
     """
     Generator approach to read files
@@ -16,15 +18,15 @@ def log_reader(log_file):
     """
     with open(log_file) as f:
         # version with yield from
-        yield from f.readlines()
+        #yield from f.readlines()
         # version where we return one line at the time
-        #while True:
-        #    line = f.readline()
-        #    if not line:
-        #        break
-        #    yield line
+        while True:
+           line = f.readline()
+           if not line:
+               break
+           yield line
 
-
+@profile
 def parse(log_file):
     for line in log_reader(log_file):
         if 'Failed password for root from' in line:
@@ -36,14 +38,15 @@ def parse(log_file):
                         'country_name': None
                     }
             }
-            try:
-                response = requests.get(("{}{}".format(endpoint, check.group('ip')))).json()
-            except:
-                pass
+            # try:
+            #     response = requests.get(("{}{}".format(endpoint, check.group('ip')))).json()
+            # except:
+            #     pass
             date, ip, country = check.group('date'), check.group('ip'), response.get('data').get('country_name')
             print(date, ip, country)
 
 
+@profile
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--log-file", type=str, help="Path to auth log file", required=True)
